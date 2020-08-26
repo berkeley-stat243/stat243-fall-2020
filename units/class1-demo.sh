6c5bd74a70356257f265cccc99b5df503096e1cf
Christopher Paciorek <paciorek@stat.berkeley.edu>
2020-08-26 11:13:41 -0700

    added history of commands used in Class 1

---
+++ units/class1-demo.sh
@@ -0,0 +1,40 @@
+  516  clear
+  517  ls /
+  518  ls /Users
+  519  ls
+  520  ls stat243-fall-2020
+  521  rm -rf stat243-fall-2020
+  522  git clone https://github.com/berkeley-stat243/stat243-fall-2020
+  523  ls stat243-fall-2020
+  524  cd stat243-fall-2020
+  525  git log
+  526  ls
+  527  cd units
+  528  ls
+  529  emacs -nw unit2-dataTech.R
+  530  git commit -am'added an empty line as a demo'
+  531  git push
+  532  git log
+  533  ls 
+  534  ls -l
+  535  ls -a
+  536  ls -a -l
+  537  ls -al
+  538  man ls
+  539  ssh radagast.berkeley.edu
+  540  ls /
+  541  ls /Users
+  542  ls /Library
+  543  ls /Users/paciorek
+  544  getwd
+  545  pwd
+  546  cd ..
+  547  cd units
+  548  cd ../data
+  549  ls
+  550  pwd
+  551  cd /Users/paciorek/Desktop/stat243-fall-2020/units
+  552  pwd
+  553  cd ~/Desktop/stat243-fall-2020/units
+  554  cd ~/Desktop/stat243-fall-2020/data
+  555  history >> ../units/class1-demo.sh
