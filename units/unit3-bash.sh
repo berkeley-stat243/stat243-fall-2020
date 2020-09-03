565f0214bc743dc6f568c5ea9daad1a3e7c5a488
Christopher Paciorek <paciorek@stat.berkeley.edu>
2020-09-03 09:27:04 -0700

    updated unit3

--- units/unit3-bash.sh
+++ units/unit3-bash.sh
@@ -28,7 +28,7 @@ sed -i 's/,/;/g' file.txt  # replace commas with semicolons in file.txt
 git clone https://github.com/berkeley-stat243/stat243-fall-2019
 
 ## @knitr data                
-cd stat243-fall-2018/data
+cd stat243-fall-2019/data
 gunzip coop.txt.gz
 cut -b50-70 coop.txt | less 
 cut -b60-61 coop.txt | sort | uniq
@@ -38,6 +38,10 @@ cut -b60-61 coop.txt | sort | uniq -c
 tail -n 1 cpds.csv | grep -o ',' | wc -l
 nfields=$(tail -n 1 cpds.csv | grep -o ',' | wc -l)
 
+nfields=$((${nfields}+1))
+echo $nfields
+
+## alternatively, we can use `bc`
 nfields=$(echo "${nfields}+1" | bc)
 
 ## @knitr mission3             
@@ -53,6 +57,20 @@ ls -tr *.R | tail -n 5 | xargs grep -l 'example.pdf'
 grep -l 'example.pdf' $(ls -tr *.R | tail -n 5)
 
 ## @knitr mission4
+
+ls -rt ~/Downloads | tail -n 1
+
+## sometimes the ~ behaves weirdly in scripting, so let's use full path 
+mv "/accounts/gen/vis/paciorek/Downloads/$(ls -rt \
+   /accounts/gen/vis/paciorek/Downloads | tail -n 1)" ~/Desktop
+
+function mvlast() {
+    mv "/accounts/gen/vis/paciorek/Downloads/$(ls -rt \
+       /accounts/gen/vis/paciorek/Downloads | tail -n $2)" $1
+}
+
+
+## @knitr mission5
 grep library unit[1-9]*.R
 grep --no-filename library *.R
 grep --no-filename "^library" *.R
@@ -72,18 +90,18 @@ unique packages we will install."
 ## unique packages we will install."
 
 
-## @knitr mission4a               -
+## @knitr mission5a               -
 Rscript -e "pkgs <- scan('libs.txt', what = 'character'); \
 install.packages(pkgs, repos = 'https://cran.cnr.berkeley.edu')"
 
-## @knitr mission5               
+## @knitr mission6             
 echo "Sys.sleep(1e5)" > job.R 
 nJobs=30 
 for (( i=1; i<=${nJobs}; i++ )); do     
    R CMD BATCH --no-save job.R job-${i}.out & 
 done 
 
-## @knitr mission5a          
+## @knitr mission6a          
 ps -o pid,pcpu,pmem,user,cmd -C R 
 ps -o pid,pcpu,pmem,user,cmd,start_time --sort=start_time -C R | tail -n 30
 ps -o pid --sort=start_time -C R | tail -n ${nJobs} | xargs kill
@@ -94,37 +112,4 @@ ps -o pid,pcpu,pmem,user,command | grep exec/R
 ps -o pid,command | grep exec/R | cut -d' ' -f1 |  tail -n ${nJobs} | xargs kill
 
 
-grep bash unit4-bash.sh
 
-grep -c bash unit4-bash.sh
-
-num=$(grep -c bash unit4-bash.sh)
-echo "There are ${num} occurrences of the word 'bash' in this file."
-
-echo There are $(grep -c bash unit4-bash.sh) occurrences of the word 'bash' in this file.
-
-# For Belgium, determine the minimum unemployment value (field #6) in cpds.csv in a pro- grammatic way.
-
-grep Belgium cpds.csv | cut -d',' -f6  | sort -n 
-
-# Have what is printed out to the screen look like “Belgium 6.2”.
-
-# Now store the unique values of the countries in a variable, first stripping out the quotation marks and removing the space in “New Zealand”, which causes problems because of the space in its name.
-
-countries=$(cut -d',' -f2 cpds.csv | tail -n +2 | sort -u | sed 's/"//g') 
-
-# Figure out how to automate step 1 to do the calculation for all the countries and print to the screen.
-
-for c in ${countries}; do
-    echo ${c} $(grep ${c} cpds.csv | cut -d',' -f6  | sort -n | head -n +2)
-done
-
-# First, take the fourth column. Figure out the unique values in that column.
-# Next, automate the process of determining if any of the values are non-numeric so that you don’t have to scan through all of the unique values looking for non-numbers. You’ll need to look for the following regular expression pattern “[^0-9]”, which is interpreted as NOT any of the numbers 0 through 9.
-
-cut -d',' -f4 RTADataSub.csv | sort | uniq 
-
-nfields=60
-for (( i=2; i <= ${nfields}; i++ )); do
-    echo "field number $i $(cut -d',' -f${i} RTADataSub.csv | sort | uniq)"
-    done
