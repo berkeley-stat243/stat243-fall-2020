70a2dd4e03bfb3eafdd39b35a2db3fff280f2c67
zoevernon <zoe_vernon@berkeley.edu>
2020-09-24 11:23:05 -0700

    updated

--- sections/05/logitBoot.R
+++ sections/05/logitBoot.R
@@ -1,21 +1,40 @@
-myData <- read.csv('./data.csv')
+my_data <- read.csv('./data.csv')
 
-logitBoot <- function(y, x, nBoot = 2000) {
+logitBoot <- function(y, x, n_boot = 2000) {
   set.seed(5)
-  out <- sapply(seq_len(nBoot), myglm, y, x)
-  boot_se <- sd(out)
+  
+  # do n_boot random permutations of x and y and return coefficient on x with 
+  # the myGLM function
+  boot_coefs <- sapply(seq_len(n_boot), myGLM, y, x)
+  
+  # compute standard deviation of those estimates and return
+  boot_se <- sd(boot_coefs)
   return(boot_se)
 }
 
-myglm <- function(i, y, x) {
+myGLM <- function(i, y, x) {
   n <- length(y)
-  ind <- sample(seq_len(n), n, replace = TRUE)
-  out <- glm(y[ind]~x[ind], family='binomial')
-  return(out$coef[2])
+  
+  # randomly sample with replacement from the observations in the data
+  boot_sample <- sample(seq_len(n), n, replace = TRUE)
+  
+  # create vectors of the bootstrapped samples
+  x_boot <- x[boot_sample]
+  y_boot <- y[boot_sample]
+  
+  # fit logistic regression on permutated data
+  mod_boot <- glm(y_boot ~ x_boot, family = 'binomial')
+  
+  # return the estimated coefficient
+  return(mod_boot$coef[2])
 }
 
-mod <- glm(y ~ x, data = myData, family = 'binomial')
-summary(mod)
-## note that the standard error for the regression coefficient is ~3
+# fit model in R
+mod <- glm(y ~ x, data = my_data, family = 'binomial')
 
-logitBoot(myData$y, myData$x)
+# note that the standard error for the regression coefficient is ~3
+summary(mod)
+
+# estimate standard error with our bootstrap function
+# note the overestimation of standard error 119 > 3
+logitBoot(my_data$y, my_data$x)
