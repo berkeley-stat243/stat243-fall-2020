4f3392b5ab7820e8e7e016536d7e2cf9d0492d84
Christopher Paciorek <paciorek@stat.berkeley.edu>
2020-09-28 09:21:04 -0700

    updated unit5 section 7.3

--- units/unit5-programming.R
+++ units/unit5-programming.R
@@ -1233,21 +1233,53 @@ f1(wgtsB, IDsB, muB)
 
 
 ## @knitr challenge5
-for (i in 1:n) {        
-  for (j in 1:n) {
-    for (z in 1:K) { 
-       if (theta.old[i, z]*theta.old[j, z] == 0){ 
-          q[i, j, z] <- 0 
-       } else { 
-          q[i, j, z] <- theta.old[i, z]*theta.old[j, z] /
-               Theta.old[i, j] 
-       } 
-    } 
-  }
+
+ll <- function(Theta, A) {
+  sum.ind <- which(A==1, arr.ind=T)
+  logLik <- sum(log(Theta[sum.ind])) - sum(Theta)
+  return(logLik)
 }
-theta.new <- theta.old 
-for (z in 1:K) { 
-   theta.new[,z] <- rowSums(A*q[,,z])/sqrt(sum(A*q[,,z])) 
+
+oneUpdate <- function(A, n, K, theta.old, thresh = 0.1) { 
+  theta.old1 <- theta.old
+  Theta.old <- theta.old %*% t(theta.old)
+  L.old <- ll(Theta.old, A)
+  q <- array(0, dim = c(n, n, K))
+  
+  for (i in 1:n) {
+    for (j in 1:n) {
+      for (z in 1:K) {
+        if (theta.old[i, z]*theta.old[j, z] == 0){
+          q[i, j, z] <- 0
+        } else {
+          q[i, j, z] <- theta.old[i, z]*theta.old[j, z] /
+            Theta.old[i, j]
+        }
+      }
+    }
+  }
+  theta.new <- theta.old
+  for (z in 1:K) {
+    theta.new[,z] <- rowSums(A*q[,,z])/sqrt(sum(A*q[,,z]))
+  }
+  Theta.new <- theta.new %*% t(theta.new)
+  L.new <- ll(Theta.new, A)
+      converge.check <- abs(L.new - L.old) < thresh
+  theta.new <- theta.new/rowSums(theta.new)
+  return(list(theta = theta.new, loglik = L.new,
+              converged = converge.check)) 
+}
+
+# initialize the parameters at random starting values
+temp <- matrix(runif(n*K), n, K)
+theta.init <- temp/rowSums(temp)
+
+# do single update
+out <- oneUpdate(A, n, K, theta.init)
+
+
+# in the real code, oneUpdate was called repeatedly in a while loop
+# as part of an iterative optimization to find a maximum likelihood estimator
 } 
 
 ## @knitr challenge6
